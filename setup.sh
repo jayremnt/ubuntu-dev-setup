3a4cbf24c722f08ca0e35320ab56c51fe0115ee7
Jayremnt <jayremnt@gmail.com>
2023-04-23 23:06:37 +0700

    feat: install Discord

--- setup.sh
+++ setup.sh
@@ -57,6 +57,11 @@ curl -sS https://download.spotify.com/debian/pubkey_7A3A762FAFD4A51F.gpg | sudo 
 echo "deb http://repository.spotify.com stable non-free" | sudo tee /etc/apt/sources.list.d/spotify.list
 sudo apt-get update && sudo apt-get install -y spotify-client
 
+# Install Discord
+echo "Installing Discord"
+sudo apt update && sudo apt upgrade -y
+sudo snap install discord
+
 # Install Tweaks
 sudo apt install gnome-tweaks
 
