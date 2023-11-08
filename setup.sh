1587419aecbd4798e14ee99871de37a3b4c63780
Jayremnt <jayremnt@gmail.com>
2023-11-08 07:48:46 +0700

    fix: install Spotify using snap

--- setup.sh
+++ setup.sh
@@ -63,9 +63,7 @@ gnome-terminal -- bash -c "/opt/WebStorm/WebStorm-*/bin/webstorm.sh"
 # TODO: create desktop entry
 
 echo "Installing Spotify..."
-curl -sS https://download.spotify.com/debian/pubkey_7A3A762FAFD4A51F.gpg | sudo gpg --dearmor --yes -o /etc/apt/trusted.gpg.d/spotify.gpg
-echo "deb http://repository.spotify.com stable non-free" | sudo tee /etc/apt/sources.list.d/spotify.list
-sudo apt-get update && sudo apt-get install -y spotify-client
+sudo snap install spotify
 
 echo "Installing Discord..."
 sudo apt update && sudo apt upgrade -y
