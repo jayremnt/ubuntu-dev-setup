872a8a458f14d4d603c37927ea7dc2e38063161a
Jayremnt <jayremnt@gmail.com>
2024-04-08 13:49:39 +0700

    feat: install SF fonts

--- install_macos_theme.sh
+++ install_macos_theme.sh
@@ -7,10 +7,12 @@ GNOME_SHELL_EXTENSIONS=(
   gnome-ui-tune@itstime.tech
   user-theme@gnome-shell-extensions.gcampax.github.com
 )
-WHITESUR_CONFIG_DIR=$HOME/projects/others/whitesur
-WHITESUR_THEME_DIR=$WHITESUR_CONFIG_DIR/WhiteSur-gtk-theme
-WHITESUR_WALLPAPERS_DIR=$WHITESUR_CONFIG_DIR/WhiteSur-wallpapers
-WHITESUR_ICON_THEME=$WHITESUR_CONFIG_DIR/WhiteSur-icon-theme
+MACOS_CONFIG_DIR=$HOME/projects/others/ubuntu-to-macos-theme
+WHITESUR_THEME_DIR=$MACOS_CONFIG_DIR/WhiteSur-gtk-theme
+WHITESUR_WALLPAPERS_DIR=$MACOS_CONFIG_DIR/WhiteSur-wallpapers
+WHITESUR_ICON_THEME_DIR=$MACOS_CONFIG_DIR/WhiteSur-icon-theme
+SF_PRO_FONTS_DIR=$MACOS_CONFIG_DIR/San-Francisco-Pro-Fonts
+SF_MONO_FONT_DIR=$MACOS_CONFIG_DIR/SF-Mono-Font
 DOWNLOADS_DIR=$HOME/Downloads
 
 # Download extensions
@@ -28,8 +30,8 @@ $WHITESUR_THEME_DIR/install.sh -c Dark -t purple -m -N stable -l --normal --roun
 sudo $WHITESUR_THEME_DIR/tweaks.sh -g -N
 
 # Set WhiteSur icon theme
-git clone https://github.com/vinceliuice/WhiteSur-icon-theme.git $WHITESUR_ICON_THEME
-$WHITESUR_ICON_THEME/install.sh
+git clone https://github.com/vinceliuice/WhiteSur-icon-theme.git $WHITESUR_ICON_THEME_DIR
+$WHITESUR_ICON_THEME_DIR/install.sh
 
 # Set WhiteSur dynamic wallpaper
 git clone https://github.com/vinceliuice/WhiteSur-wallpapers.git $WHITESUR_WALLPAPERS_DIR
@@ -38,9 +40,15 @@ sudo $WHITESUR_WALLPAPERS_DIR/install-gnome-backgrounds.sh
 # Set MacOS cursor
 wget -P $DOWNLOADS_DIR -O $DOWNLOADS_DIR/apple_cursor.tar.gz https://github.com/ful1e5/apple_cursor/releases/download/v2.0.0/macOS-BigSur-White.tar.gz
 tar -xvf $DOWNLOADS_DIR/apple_cursor.tar.gz -C $DOWNLOADS_DIR
-mv $DOWNLOADS_DIR/macOS-* ~/.icons/
+cp -rf $DOWNLOADS_DIR/macOS-* $HOME/.icons/
 
-# Config Tweaks
+# Install SF Pro fonts
+git clone https://github.com/sahibjotsaggu/San-Francisco-Pro-Fonts.git $SF_PRO_FONTS_DIR
+git clone https://github.com/supercomputra/SF-Mono-Font.git $SF_MONO_FONT_DIR
+mkdir -p $HOME/.fonts
+cp $SF_PRO_FONTS_DIR/SF-Pro-Rounded-Regular.otf $SF_MONO_FONT_DIR/SFMono-Regular.otf $HOME/.fonts
+
+# Play with Tweaks
 dconf write /org/gnome/desktop/interface/icon-theme "'WhiteSur'"
 dconf write /org/gnome/shell/extensions/user-theme/name "'WhiteSur-Dark-solid-purple'"
 dconf write /org/gnome/desktop/interface/gtk-theme "'WhiteSur-Dark-purple'"
@@ -50,7 +58,11 @@ dconf write /org/gnome/desktop/interface/clock-show-date true
 dconf write /org/gnome/desktop/interface/clock-show-seconds true
 dconf write /org/gnome/desktop/wm/preferences/button-layout "'close,minimize,maximize:'"
 dconf write /org/gnome/desktop/interface/cursor-theme "'macOS-BigSur-White'"
+dconf write /org/gnome/desktop/interface/font-name "'SF Pro Rounded 11'"
+dconf write /org/gnome/desktop/interface/document-font-name "'SF Pro Rounded 11'"
+dconf write /org/gnome/desktop/interface/monospace-font-name "'SF Mono 13'"
+dconf write /org/gnome/desktop/wm/preferences/titlebar-font "'SF Pro Rounded Bold 11'"
 
 # Config Dock
 dconf write /org/gnome/shell/extensions/dash-to-dock/show-show-apps-button false
-dconf write /org/gnome/shell/favorite-apps ['org.gnome.Nautilus.desktop', 'google-chrome.desktop', 'jetbrains-webstorm.desktop', 'org.gnome.Settings.desktop']
+dconf write /org/gnome/shell/favorite-apps "['org.gnome.Nautilus.desktop', 'google-chrome.desktop', 'jetbrains-webstorm.desktop', 'org.gnome.Settings.desktop']"
